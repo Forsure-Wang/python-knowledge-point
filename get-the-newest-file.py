13591dae7caf61d778d773966da293f23218b45c
Forsure <wangfengshuo2012@163.com>
2019-12-09 17:13:23 +0800

    Deal with null directory list.

--- get-the-newest-file.py
+++ get-the-newest-file.py
@@ -11,12 +11,16 @@ def get_file_list(file_path):
 
     # os.path.getmtime()
     # os.path.getctime()
-    sorted_dir_list = sorted(postgres_dir_list, key=lambda x: os.path.getctime(os.path.join(file_path, x)), reverse=True)
-    print "sorted by modified time of the files:\n {0} \n".format(sorted_dir_list)
-    print sorted_dir_list[0]
+    if postgres_dir_list:
+        sorted_dir_list = sorted(postgres_dir_list, key=lambda x: os.path.getctime(os.path.join(file_path, x)), reverse=True)
+        print "sorted by modified time of the files:\n {0} \n".format(sorted_dir_list)
+        return sorted_dir_list[0]
+    else:
+        return []
 
 if __name__ == "__main__":
-
-    get_file_list('/home/w15021/')
-    
-# Get the newest file with given filter in specified directory
+    list = get_file_list('/home/w15021/')
+    if list:
+        print "Y"
+    else:
+        print "N"
